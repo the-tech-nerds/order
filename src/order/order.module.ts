1b5fdb545dfac1d784715e3ef2189d9bb1c24ec1
Sakibur <sakibur@sixads.net>
2021-06-14 21:27:31 +0600

    Event Based Communiation on product inventory update

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,15 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiResponseService } from '@the-tech-nerds/common-services';
 import { OrderController } from './controller/order.controller';
 import { StudentController } from './controller/student.controller';
+import { Inventory } from './entities/inventory.entity';
 import { Order } from './entities/order.entity';
 import { Student } from './entities/student.entity';
+import InventoryUpdateEvent from './events/inventory-update.event';
 import { CreateOrderService } from './service/create-order.service';
 import { StudentService } from './service/student.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, Order])],
+  imports: [TypeOrmModule.forFeature([Student, Order, Inventory])],
   providers: [ApiResponseService, StudentService, CreateOrderService],
-  controllers: [StudentController, OrderController],
+  controllers: [StudentController, OrderController, InventoryUpdateEvent],
   exports: [],
 })
 export class OrderModule {}
