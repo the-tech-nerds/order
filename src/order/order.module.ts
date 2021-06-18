767fe41553467ce69e991fee5d5735c929675b5c
rahimin <rahiminmyself@gmail.com>
2021-06-18 22:27:02 +0600

    order api

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiResponseService } from '@the-tech-nerds/common-services';
 import { OrderController } from './controller/order.controller';
-import { StudentController } from './controller/student.controller';
+
 import { Inventory } from './entities/inventory.entity';
 import { Order } from './entities/order.entity';
-import { Student } from './entities/student.entity';
 import InventoryUpdateEvent from './events/inventory-update.event';
 import { CreateOrderService } from './service/create-order.service';
-import { StudentService } from './service/student.service';
+import { OrderStatusService } from './service/order-status.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, Order, Inventory])],
-  providers: [ApiResponseService, StudentService, CreateOrderService],
-  controllers: [StudentController, OrderController, InventoryUpdateEvent],
+  imports: [TypeOrmModule.forFeature([Order, Inventory])],
+  providers: [ApiResponseService, OrderStatusService, CreateOrderService],
+  controllers: [OrderController, InventoryUpdateEvent],
   exports: [],
 })
 export class OrderModule {}
